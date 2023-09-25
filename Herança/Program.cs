34712f26a218917628fa1a94fba15bb87317ac6f
Matheus <mat.araujoribeiro@gmail.com>
2023-09-24 21:07:13 -0300

    herança

--- Herança/Program.cs
+++ Herança/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Herança!\n--------------\n");
+
+var t = new Aluno();
+t.Identificar();
+
+public class Pessoa
+{
+    public string? Nome { get; set; }
+    public string? Email { get; set; }
+    public void Identificar()
+    {
+        Console.WriteLine($"Nome: {Nome} - Email: {Email}");
+    }
+}
+
+public class Funcionario : Pessoa
+{
+    public string? Empresa { get; set; }
+    public decimal Salario { get; set; }
+}
+
+public class Aluno : Pessoa
+{
+    public string? Curso { get; set; }
+    public int Nota { get; set; }
+}
