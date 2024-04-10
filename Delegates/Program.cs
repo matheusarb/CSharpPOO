705aa60a0d3458751af353830ebdb7cd09a5062e
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-04-09 23:05:03 -0300

    delegats

---
+++ Delegates/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("-----Delegates-----");
+
+// Conceito: Delegate é um tipo que representa referências a métodos com uma lista de parâmetros
+// e um tipo de retorno específicos
