f61e14428735cd712325da9cc5d4f8470b523401
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-04-10 12:47:37 -0300

    single e multicast delegate

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -8,26 +8,37 @@ Console.WriteLine("-----Delegates-----\n");
 
 // Formas de declaração
 var resultado = Calculadora.Somar(2, 2);
-Console.WriteLine(resultado);
+// Console.WriteLine(resultado);
 
 DelegateCalculadora calc = new DelegateCalculadora(Calculadora.Somar);
 var resultado1 = calc.Invoke(20, 30);
-Console.WriteLine(resultado1);
+// Console.WriteLine(resultado1);
 
 DelegateCalculadora calc2 = Calculadora.Somar;
 var resultado2 = calc2.Invoke(120, 150);
-Console.WriteLine(resultado2);
+// Console.WriteLine(resultado2);
 
 DelegateCalculadora calc3 = (float x, float y) => Calculadora.Somar(x, y);
 var resultado3 = calc3.Invoke(1000, 1500);
-Console.WriteLine(resultado3);
+// Console.WriteLine(resultado3);
 
+// 1. SingleCast Delegate 
 //Forma mais simples
 DelegateCalculadora calc4 = Calculadora.Subtrair;
 var resultado4 = calc4.Invoke(120, 150);
-Console.WriteLine(resultado4);
+// Console.WriteLine(resultado4);
 
 Console.ReadKey();
 
-// SingleCast Delegate 
+
+// 2. MultiCast Delegate
+DelegateCalculadora delegateMulticast = Calculadora.Somar;
+delegateMulticast += Calculadora.Subtrair;
+var resultado5 = delegateMulticast(10, 5);
+System.Console.WriteLine(resultado5);
+
+
+// 3. Funções anonimas
+
 public delegate float DelegateCalculadora(float x, float y);
+
