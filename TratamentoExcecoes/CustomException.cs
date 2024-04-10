2c3793177c353499d3f92fa3775ae3249a8defac
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-04-09 23:04:58 -0300

    tratamento excecoes

---
+++ TratamentoExcecoes/CustomException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TratamentoExcecoes;
+
+internal class CustomException : Exception
+{
+    public CustomException()
+    {
+        
+    }
+    
+    public CustomException(string message)
+        :base(message)
+    {
+        Console.WriteLine("Exceção personalizada");
+    }
+    
+    public CustomException(string message, Exception inner)
+        : base(message, inner)
+    {
+        Console.WriteLine("Exceção personalizada");
+        Console.WriteLine(inner.Message);
+        Console.WriteLine(inner.StackTrace);
+        Console.WriteLine(inner.Source);
+    }
+}
---
+++ TratamentoExcecoes/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("-----Tratamento Exceções-----\n");
+
+var httpClient = new HttpClient();
+
+var uri = "https://macoratti.net/dados/poesia.txt";
+var response = httpClient.GetAsync(uri).Result;
+
+
+try
+{
+    if(response.IsSuccessStatusCode)
+    {
+        Console.WriteLine("Arquivo acessado");
+        Console.WriteLine("Código de status" + response.StatusCode);
+    }
+}
+catch (HttpRequestException ex) when (ex.Message.Contains("404"))
+{
+    Console.WriteLine("Página não encontrada");
+    Console.WriteLine(response.StatusCode);
+}
+catch (HttpRequestException ex) when (ex.Message.Contains("500"))
+{
+    Console.WriteLine("Página não encontrada");
+    Console.WriteLine(response.StatusCode);
+}
+catch (HttpRequestException ex) when (ex.Message.Contains("404"))
+{
+    Console.WriteLine("Página não encontrada");
+    Console.WriteLine(response.StatusCode);
+}
+catch (HttpRequestException ex) when (ex.Message.Contains("404"))
+{
+    Console.WriteLine("Página não encontrada");
+    Console.WriteLine(response.StatusCode);
+}
+//catch(HttpRequestException ex)
+//catch (Excepti)
+//{
+
+//}
