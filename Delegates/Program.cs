57980eea228c84c589f2dfa5ed5024b0c8bd1e69
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-04-10 13:13:31 -0300

    funcoes anonimas e expr lambda

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -28,17 +28,37 @@ DelegateCalculadora calc4 = Calculadora.Subtrair;
 var resultado4 = calc4.Invoke(120, 150);
 // Console.WriteLine(resultado4);
 
-Console.ReadKey();
+// Console.ReadKey();
 
 
 // 2. MultiCast Delegate
 DelegateCalculadora delegateMulticast = Calculadora.Somar;
 delegateMulticast += Calculadora.Subtrair;
 var resultado5 = delegateMulticast(10, 5);
-System.Console.WriteLine(resultado5);
+// System.Console.WriteLine(resultado5);
 
 
 // 3. Funções anonimas
+// 3.1. Métodos Anônimos
+Imprimir imprimir = delegate (int valor)
+{
+    System.Console.WriteLine("método anônimo. Valor: "+valor);
+};
+// imprimir(100);
+var list = new List<string>{"matheus", "luiggi"};
+// string result = list.Find(delegate (string nome){
+//     var n = "Luiggi";
+//     System.Console.WriteLine("Consultando o nome: "+nome);
+//     return nome.Equals(n, StringComparison.OrdinalIgnoreCase);
+// });
+// System.Console.WriteLine(result);
 
+// Expressões Lambda
+// Criada a partir de delegates e métodos anônimos
+// É uma maneira mais curta de representar um método anônimo usando uma sintaxe especial (sugar syntax)
+var result = list.Find(nome => nome.Equals("luiggi"));
+System.Console.WriteLine(result);
+
+
+public delegate void Imprimir(int valor);
 public delegate float DelegateCalculadora(float x, float y);
-
