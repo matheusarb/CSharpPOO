1ec733f8ce5fd5a53e6a27ee7c77034031b76541
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-04-11 15:06:58 -0300

    serialization json

--- SerializacaoDesserializacao/Program.cs
+++ SerializacaoDesserializacao/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
 using System.Xml.Serialization;
 
 Console.WriteLine("---Serialização e Desserialização---\n");
@@ -6,12 +7,39 @@ Console.WriteLine("---Serialização e Desserialização---\n");
 
 // I. Serialização Binária (OBSOLETA)
 
-// II. Serialização XML
+// II.1 Serialização JSON
 // Precisamos de um construtor público parameterless
-var aluno = new Aluno(1, "Matheus", "matheus@io.com", 32);
-var caminhoArquivo = @"C:\dev\Serializacao\AlunoSerializado.bin";
-var serializer = new XmlSerializer(typeof(Aluno));
+var aluno1 = new Aluno(1, "Matheus", "matheus@io.com", 32);
+var caminhoArquivo1 = @"C:\dev\Serializacao\AlunoSerializado.json";
 
-using (var writer = new StreamWriter(caminhoArquivo)){
-    serializer.Serialize(writer, aluno);
-}
+// using(var stream = new FileStream(caminhoArquivo1, 
+//                     FileMode.OpenOrCreate,
+//                     FileAccess.ReadWrite)){
+//     JsonSerializer.Serialize(stream, aluno1);
+// }
+
+// II.1.1. Desserialização JSON
+var jsonContent = File.ReadAllText(caminhoArquivo1);
+var aluno = JsonSerializer.Deserialize<Aluno>(jsonContent);
+foreach(var prop in aluno.GetType().GetProperties())
+    System.Console.WriteLine($"{prop.Name} - {prop.GetValue(aluno)}");
+
+
+// II.2. Serialização XML
+// Precisamos de um construtor público parameterless
+var caminhoArquivo2 = @"C:\dev\Serializacao\AlunoSerializado.xml";
+
+//Serializar
+// using (var writer = new StreamWriter(caminhoArquivo)){
+//     serializer.Serialize(writer, aluno1);
+// }
+
+// II.2.1. Desserialização
+// using(var reader = new StreamReader(caminhoArquivo2)){
+//     var aluno = (Aluno)serializer.Deserialize(reader);
+//     System.Console.WriteLine(@$"Aluno XML desserializado:
+//         Id - {aluno.Id}
+//         Nome - {aluno.Nome}
+//         Email - {aluno.Email}
+//         Idade - {aluno.Idade}");
+// }
