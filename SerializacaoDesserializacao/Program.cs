48ebd86fb64765e6164f57ea6ea040da44927241
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-04-11 14:48:12 -0300

    add serialization

---
+++ SerializacaoDesserializacao/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Serialization;
+
+Console.WriteLine("---Serialização e Desserialização---\n");
+
+
+// I. Serialização Binária (OBSOLETA)
+
+// II. Serialização XML
+// Precisamos de um construtor público parameterless
+var aluno = new Aluno(1, "Matheus", "matheus@io.com", 32);
+var caminhoArquivo = @"C:\dev\Serializacao\AlunoSerializado.bin";
+var serializer = new XmlSerializer(typeof(Aluno));
+
+using (var writer = new StreamWriter(caminhoArquivo)){
+    serializer.Serialize(writer, aluno);
+}
---
+++ SerializacaoDesserializacao/Serializacao.cs
@@ -0,0 +1,39 @@
+//Conceito: serialização é o processo de conversão do estado de um objeto em um formato que possa
+// ser armazenado e/ou transportado num arquivo, banco de dados ou memória
+
+// Trata-se da conversão de um objeto em um fluxo de bytes(stream), de forma que
+// possa ser armazenado na memória, em arquivo ou em um banco de dados
+
+// Modos de serialização
+// I. Serialização Binária (OBSOLETO)
+// Preserva fidelidade do tipo e estado de um objeto
+// System.Runtime.Serialization
+
+// II. Serialização XML ou SOAP
+// Serializa somente propriedades públicas e não preserva a fidelidade de tipo
+// System.Xml.Serialization
+
+// III. Serialização JSON
+// Serializa somente propriedades públicas e não preserva a fidelidade de tipo
+// System.Text.Json (vem substituindo a NewtonSoft por melhor desempenho e é nativo do .NET)
+// NewtonSoft.Json
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    
+    [NonSerialized]
+    public int Idade;
+
+    public Aluno(){}
+
+    public Aluno(int id, string nome, string email, int idade)
+    {
+        Id = id;
+        Nome = nome;
+        Email = email;
+        Idade = idade;
+    }
+}
