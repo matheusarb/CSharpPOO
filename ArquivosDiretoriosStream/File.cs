fea4cf3f6fafb88820e957eef5281732a3225775
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-04-11 10:51:13 -0300

    File class

---
+++ ArquivosDiretoriosStream/File.cs
@@ -0,0 +1,3 @@
+// Classe File
+
+// I. Dentro de System.IO e é usada para realizar operações de leitura e gravação de arquivos
--- ArquivosDiretoriosStream/Program.cs
+++ ArquivosDiretoriosStream/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("----\tArquivos, Diretórios, Stream\t----\n");
+
+// I. C# fornece diversas classes para trabalhar com arquivos:
+// File, FileInfo, Directory, DirectoryInfo, Path, FileStream, StreamReader, StreamWriter
+
+// II. Métodos:
+// II.1. Criação e manipulação
+// Create, Delete, Exists, Copy, Move
+// II.1. Leitura e escrita
+// ReadAllText, ReadAllBytes, WriteAllText, WriteAllBytes, AppendAllText
+
+// III. Criar arquivos:
+// Para criar é preciso especificar o caminho e o nome do arquivo
+
+// IV. Classe FILE na prática:
+var caminhoOrigem = @"C:\dev\arquivo.txt";
+//criar arquivo
+File.Create(caminhoOrigem);
+File.WriteAllText(caminhoOrigem, "Criando primeiro arquivo");
